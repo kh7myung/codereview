9ec79719da472913b45692ae86270fef29bb69b9
kh7myung <51574789+kh7myung@users.noreply.github.com>
2019-06-12 15:08:46 +0900

    Update FindKthElement.java

--- FindKthElement.java
+++ FindKthElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ * Copyright (C) 2014 Pedro Vicente G처mez S찼nchez.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,87 +14,89 @@
  * limitations under the License.
  */
 /**
- * Implement an algorithm to find the kth to last element of a singly linked list. Can you do it using recursion?
+ * Implement an algorithm to find the kth to last element of a singly linked
+ * list. Can you do it using recursion?
  *
- * @author Pedro Vicente Gómez Sánchez.
+ * @author Pedro Vicente G처mez S찼nchez.
  */
 public class FindKthElement {
 
-  private ReverseLinkedList reverseLinkedList;
+	private ReverseLinkedList reverseLinkedList;
 
-  public FindKthElement() {
-    this.reverseLinkedList = new ReverseLinkedList();
-  }
+	public FindKthElement() {
+		this.reverseLinkedList = new ReverseLinkedList();
+	}
 
-  /**
-   * Iterative algorithm based on reverse the singly linked list and iterate over the reversed list to find the kth element.
-   * This algorithm has a complexity order in time terms equals to O(N) where N is the number of elements in the list.
-   * Remember that our reverse algorithm also has O(N) as complexity order.
-   * In space terms we get O(1) because we are not using any auxiliary data structure.
-   */
-  public ListNode Find(ListNode listNode, int k) {
-    ListNode reversedList = reverseLinkedList.reverseIterative(listNode);
-    ListNode result = reversedList;
-    while (k > 0)
-    {
-      result = result.getNext();
-      if (result == null)
-        throw new IndexOutOfBoundsException();
-      k--;
-    }
-    return result;
-  }
+	/**
+	 * Iterative algorithm based on reverse the singly linked list and iterate over
+	 * the reversed list to find the kth element. This algorithm has a complexity
+	 * order in time terms equals to O(N) where N is the number of elements in the
+	 * list. Remember that our reverse algorithm also has O(N) as complexity order.
+	 * In space terms we get O(1) because we are not using any auxiliary data
+	 * structure.
+	 */
+	public ListNode Find(ListNode listNode, int k) {
+		ListNode reversedList = reverseLinkedList.reverseIterative(listNode);
+		ListNode result = reversedList;
+		while (k > 0) {
+			result = result.getNext();
+			if (result == null)
+				throw new IndexOutOfBoundsException();
+			k--;
+		}
+		return result;
+	}
 
-  /**
-   * Another iterative approach based on calculate the list size before to start iterating over the list to find the Kth element.
-   * The complexity order in time and space terms is the same than the previous approach.
-   */
-  public ListNode Find2(ListNode listNode, int k) {
-    int size = CalculateListSize(listNode);
-    int length = size;
-    if (k > (size - 1)) throw new IndexOutOfBoundsException();
-    ListNode result = listNode;
-    int n = size - 1 - k;
-    // System.out.println("n=" + n);
-    while (n > 0)
-    {
-      result = result.getNext();
-      n--;
-    }
-    return result;
-  }
+	/**
+	 * Another iterative approach based on calculate the list size before to start
+	 * iterating over the list to find the Kth element. The complexity order in time
+	 * and space terms is the same than the previous approach.
+	 */
+	public ListNode Find2(ListNode listNode, int k) {
+		int size = CalculateListSize(listNode);
+		int length = size;
+		if (k > (size - 1))
+			throw new IndexOutOfBoundsException();
+		ListNode result = listNode;
+		int n = size - 1 - k;
+		// System.out.println("n=" + n);
+		while (n > 0) {
+			result = result.getNext();
+			n--;
+		}
+		return result;
+	}
 
-  /**
-   * Iterative algorithm to resolve this problem based on two pointers to avoid the usage of an auxiliary data structure.
-   * The complexity order of this algorithm is O(N) as in the previous implementations, but in space terms,
-   * the complexity order is equals to O(1).
-   */
-  public ListNode Find3(ListNode listNode, int k) {
-    ListNode pointer2 = listNode;
-    for (int i = 0; i <= k; i++)
-    {
-      if (pointer2 == null) throw new IndexOutOfBoundsException();
-      pointer2 = pointer2.getNext();
-    }
+	/**
+	 * Iterative algorithm to resolve this problem based on two pointers to avoid
+	 * the usage of an auxiliary data structure. The complexity order of this
+	 * algorithm is O(N) as in the previous implementations, but in space terms, the
+	 * complexity order is equals to O(1).
+	 */
+	public ListNode Find3(ListNode listNode, int k) {
+		ListNode pointer2 = listNode;
+		for (int i = 0; i <= k; i++) {
+			if (pointer2 == null)
+				throw new IndexOutOfBoundsException();
+			pointer2 = pointer2.getNext();
+		}
 
-    ListNode result = listNode;
-    while (pointer2 != null)
-    {
-      result = result.getNext();
-      pointer2 = pointer2.getNext();
-    }
-    return result;
-  }
+		ListNode result = listNode;
+		while (pointer2 != null) {
+			result = result.getNext();
+			pointer2 = pointer2.getNext();
+		}
+		return result;
+	}
 
-  private int CalculateListSize(ListNode listNode) {
-    int size = 0;
-    int length = 0;
-    while (listNode != null)
-    {
-      size++;
-      listNode = listNode.getNext();
-    }
-    // System.out.println("size=" + size);
-    return size;
-  }
+	private int CalculateListSize(ListNode listNode) {
+		int size = 0;
+		int length = 0;
+		while (listNode != null) {
+			size++;
+			listNode = listNode.getNext();
+		}
+		// System.out.println("size=" + size);
+		return size;
+	}
 }
